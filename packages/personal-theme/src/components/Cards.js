24087c4b4e6166d77f98c4b54d1fbd99f9a49dd1
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-07-15 12:43:55 -0700

    Building Landing Page

---
+++ packages/personal-theme/src/components/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { styled } from "frontity"
+
+export default function Cards() {
+    return (
+        <div>
+            <div className="cards">
+
+                <div className="card">
+                    <img src="" alt="" />
+                    <h2>About</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt="" />
+                    <h2>Blog</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt="" />
+                    <h2>Schedule</h2>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
